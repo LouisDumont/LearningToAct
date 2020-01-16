801ba7f9bff42803e9c8d07b406317d1382c5669
LouisDumont <dumont.louis21@gmail.com>
2020-01-16 14:20:51 +0100

    Reverting D6 to be equal to D2

--- examples/D6_navigation_multiFrame/run_exp.py
+++ examples/D6_navigation_multiFrame/run_exp.py
@@ -33,14 +33,14 @@ def main(main_args):
 	# Train experience
 	train_experience_args = {}
 	train_experience_args['memory_capacity'] = 20000 # Number of observations to retain
-	train_experience_args['history_length'] = 4 # Number of frames taken as input when making a prediction
-	train_experience_args['history_step'] = 8
+	train_experience_args['history_length'] = 1 # Number of frames taken as input when making a prediction
+	train_experience_args['history_step'] = 1
 	train_experience_args['action_format'] = 'enumerate'
 	train_experience_args['shared'] = False
 	
 	# Test prediction experience
 	test_prediction_experience_args = train_experience_args.copy()
-	test_prediction_experience_args['memory_capacity'] = 100
+	test_prediction_experience_args['memory_capacity'] = 1
 	
 	# Test policy experience
 	test_policy_experience_args = train_experience_args.copy()
